abef42a2d3e7e722258f22c65bfd31d560a7261e
Madysan Miller <madysanmiller2020@gmail.com>
2023-08-28 10:34:42 -0400

    finnished tasks

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,4 +2,11 @@ import { combineReducers } from 'redux';
 
 import movieReducer from './movieReducer';
 
-export default movieReducer;
+import FavoriteReducer from '../components/FavoriteMovieList';
+
+const rootReducer = combineReducers({
+    movieReducer,
+    FavoriteReducer
+});
+
+export default rootReducer;
